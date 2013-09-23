71a026d32e566b99b54d44f28749555c76d9db9e
shebeko@mail.ru <shebeko@mail.ru@eb107457-74e5-4d44-a7ce-0c97796e0735>
2013-09-23 13:48:02 +0000

    Add missed files
    git-svn-id: https://five-in-line.googlecode.com/svn/trunk@63 eb107457-74e5-4d44-a7ce-0c97796e0735

---
+++ algo/algo_utils.h
@@ -0,0 +1,25 @@
+#ifndef gomoku_algo_utilsH
+#define gomoku_algo_utilsH
+
+namespace Gomoku
+{
+    template<class T>
+	struct incer_t
+	{
+		T & val;
+        bool active;
+
+		incer_t(T & _val):val(_val) {++val; active=true;}
+		~incer_t(){reset();}
+
+        inline void reset()
+        {
+            if(!active)return;
+            --val;
+            active=false;
+        }
+	};
+
+}
+
+#endif
