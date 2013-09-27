3c1e68701ba9a2e9f445c33c71bd086d1e99bfac
shebeko@mail.ru <shebeko@mail.ru@eb107457-74e5-4d44-a7ce-0c97796e0735>
2013-09-27 16:31:06 +0000

    Move common php code to base files
    git-svn-id: https://five-in-line.googlecode.com/svn/trunk@81 eb107457-74e5-4d44-a7ce-0c97796e0735

--- www/var/www/fiveinline/solutions/root/solve.php
+++ www/var/www/fiveinline/solutions/root/solve.php
@@ -1,38 +1,4 @@
 <?
-$EXEC_PATH='/home/shebeko/bin/db';
 $DB_PATH='/home/shebeko/f5/root';
-
-if (count($_GET)>0) $HTTP_VARS=$_GET;
-else $HTTP_VARS=$_POST;
-
-$cmd=$HTTP_VARS["cmd"];
-
-if($cmd=='get_job')
-{
-	passthru($EXEC_PATH." ".$DB_PATH." ".escapeshellarg($cmd),$r);
-}
-else if($cmd=='save_job')
-{
-	$key=$HTTP_VARS["key"];
-	$n=$HTTP_VARS["n"];
-	$w=$HTTP_VARS["w"];
-	$f=$HTTP_VARS["f"];
-	
-	if($key==''||$n==''||$w==''||$f=='')echo "invalid params<br>";
-	else
-	{
-		passthru($EXEC_PATH." ".$DB_PATH
-			." ".escapeshellarg($cmd)
-			." ".escapeshellarg($key)
-			." ".escapeshellarg($n)
-			." ".escapeshellarg($w)
-			." ".escapeshellarg($f)
-			,$r);
-	}
-}
-else
-{
-  echo 'five in line solve database<br>';
-}
-
+include '../solve.inc';
 ?>
