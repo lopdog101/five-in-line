69ba23d9bec59c9805cc399cd53e84b8d076385f
shebeko@mail.ru <shebeko@mail.ru@eb107457-74e5-4d44-a7ce-0c97796e0735>
2013-10-04 13:30:52 +0000

    detect count of processors
    git-svn-id: https://five-in-line.googlecode.com/svn/trunk@82 eb107457-74e5-4d44-a7ce-0c97796e0735

--- f5_spawn.bat
+++ f5_spawn.bat
@@ -2,8 +2,9 @@ setlocal
 set url=http://fiveinline.info/solutions/root/solve.php
 set src_name=somebody
 set lookup_deep=2
+set process_count=%NUMBER_OF_PROCESSORS%
 
-@for /L %%i in (1,1,4) do (
+@for /L %%i in (1,1,%process_count%) do (
 @mkdir spawn\%%i
 cd spawn\%%i
 start ..\..\f5_solve.bat
