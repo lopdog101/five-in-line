8e240fbc4650687dfdc18b257adbd89b6ae63ec5
shebeko@mail.ru <shebeko@mail.ru@eb107457-74e5-4d44-a7ce-0c97796e0735>
2013-10-04 14:34:29 +0000

    Add download on WEB site
    git-svn-id: https://five-in-line.googlecode.com/svn/trunk@83 eb107457-74e5-4d44-a7ce-0c97796e0735

---
+++ www/var/www/fiveinline/index.php
@@ -0,0 +1,24 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Five-in-line project</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+<h1>Five-in-line solving tree project</h1>
+<p><a href="solutions/root/">Root database</a></p>
+<p>Project sources on <a href="https://code.google.com/p/five-in-line/">https://code.google.com/p/five-in-line/</a></p>
+<table cellSpacing="8">
+<?
+function add_version($d,$m,$y)
+{
+	echo "<tr><td>$d.$m.$y</td><td><a href=\"download/five_in_line$y$m$d.zip\">GUI Game</a><td><a href=\"download/db$y$m$d.zip\">Server</a></td></tr>";
+}
+
+add_version("04","10","2013");
+?>
+  
+</table>
+</body>
+</html>
--- www/var/www/fiveinline/solutions/root/index.php
+++ www/var/www/fiveinline/solutions/root/index.php
@@ -1,12 +1,4 @@
-﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-"http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<title>Five in line solver database</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-</head>
-<body>
-<?
+﻿<?
 $DB_PATH='/home/shebeko/f5/root';
 $OWN_PATH='http://fiveinline.info/solutions/root/';
 include '../index.inc';
