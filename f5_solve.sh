5a0a4239b9a344fe008928508dafdea8f464b5d9
shebeko@mail.ru <shebeko@mail.ru@eb107457-74e5-4d44-a7ce-0c97796e0735>
2013-10-25 13:46:09 +0000

    Port solver to ubuntu
    git-svn-id: https://five-in-line.googlecode.com/svn/trunk@99 eb107457-74e5-4d44-a7ce-0c97796e0735

--- f5_solve.sh
+++ f5_solve.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-lookup_deep=2
-export lookup_deep
-
 while true
 do
-wget -q -O key_file 'http://f5.vnetgis.com/solve.php?cmd=get_job'
+wget -q -O key_file $url'?src_name='$src_name'&cmd=get_job'
 key=`cat key_file`
 echo $key
 if [ "$key" = "" ]; then
@@ -16,5 +13,5 @@ result=`../../solver $key`
 n=`echo $result | awk '{print $2}'`
 w=`echo $result | awk '{print $3}'`
 f=`echo $result | awk '{print $4}'`
-wget -q -O result_file 'http://f5.vnetgis.com/solve.php?cmd=save_job&key='$key'&n='$n'&w='$w'&f='$f
+wget -q -O result_file $url'?src_name='$src_name'&cmd=save_job&key='$key'&n='$n'&w='$w'&f='$f
 done
--- f5_spawn.sh
+++ f5_spawn.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+url=http://fiveinline.info/solutions/root/solve.php
+src_name=somebody
+lookup_deep=2
+process_count=`cat /proc/cpuinfo | grep processor | wc -l`
+
+export url
+export src_name
+export lookup_deep
+export process_count
 
 c=0
 mkdir spawn
-while [ $c -lt 2 ]
+while [ $c -lt $process_count ]
 do
 mkdir ./spawn/$c
 cd ./spawn/$c
