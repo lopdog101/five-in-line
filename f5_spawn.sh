56f950bb95063bfc0fc89c0ade78d31352e0245d
shebeko@mail.ru <shebeko@mail.ru@eb107457-74e5-4d44-a7ce-0c97796e0735>
2013-10-25 14:45:18 +0000

    Port solver to ubuntu
    git-svn-id: https://five-in-line.googlecode.com/svn/trunk@100 eb107457-74e5-4d44-a7ce-0c97796e0735

--- f5_spawn.sh
+++ f5_spawn.sh
@@ -9,13 +9,15 @@ export src_name
 export lookup_deep
 export process_count
 
+cur_dir=`pwd`
+
 c=0
 mkdir spawn
 while [ $c -lt $process_count ]
 do
 mkdir ./spawn/$c
 cd ./spawn/$c
-daemon -f -p pid ../../f5_solve.sh
+daemon --chdir=$cur_dir/spawn/$c --pidfile=$cur_dir/spawn/$c/pid ../../f5_solve.sh
 cd ../../
 c=`expr $c + 1`
 done
