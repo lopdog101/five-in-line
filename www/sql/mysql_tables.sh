b86faeb925573c57520d54110a8226e820e85608
Eugene Shebeko <shebeko@mail.ru>
2014-12-28 07:13:28 -0500

    +optimize_tables remove state field from mysql_tables.sh

--- www/sql/mysql_tables.sh
+++ www/sql/mysql_tables.sh
@@ -2,12 +2,11 @@
 
 COUNTER=1
 while [  $COUNTER -lt 151 ]; do
-  let key_len=COUNTER*3
+  key_len=$(($COUNTER * 3)) 
 
 echo "CREATE TABLE s$COUNTER("
 echo "  k varbinary($key_len) NOT NULL DEFAULT '',"
  
-echo "  state integer NOT NULL default 0,"
 echo "  wins_count BIGINT NOT NULL default 0,"
 echo "  fails_count BIGINT NOT NULL default 0,"
 
@@ -22,5 +21,5 @@ echo ") ENGINE=MyISAM;"
 echo ""
 
 
-  let COUNTER=COUNTER+1 
+  COUNTER=$(($COUNTER + 1)) 
 done
